515a21e591013e09e7f7188492c4bacecc4b8799
Yuko Robinson <ggboyuko@gmail.com>
2020-11-27 17:09:24 +0900

    naaaa

--- js/studio_ghibuli.js
+++
@@ -1,9 +0,0 @@
-$(function(){
-	$('.hamburger').on('click', function(){
-		$(this).toggleClass('on');
-	});
-
-	$('.hamburger').on('click', function(){
-		$('.target').toggleClass('hide');
-	});
-});
