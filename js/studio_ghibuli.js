713503beb48e09b4e7be4980ea46e8bf1d18c15a
Yuko Robinson <ggboyuko@gmail.com>
2020-11-28 20:18:29 +0900

    あああああ

--- js/studio_ghibuli.js
+++ js/studio_ghibuli.js
@@ -2,12 +2,14 @@ $(function(){
 	$('#hamburger_btn').on('click', function(){
 		$(this).toggleClass('on');
     $('.drower').toggleClass('on');
-	});
-
-		$(".overlay").show();
+    });
+    
+    if(!navigator.userAgent.match(/(iPhone|iPad|iPod|Android)/)){
+        $(".overlay").show();
 		$("#modal-close").on("click",function(){
 		$(".overlay").hide();
         });
+    }
 
 	
   });
